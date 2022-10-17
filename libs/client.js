c01d78099548e664f5a57a24b431ae4da3b096e5
Ryo <ryogassk424@gmail.com>
2022-10-17 18:50:22 +0900

    🔥 envファイルを削除しました

--- libs/client.js
+++ libs/client.js
@@ -2,5 +2,5 @@ import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
   serviceDomain: "ryoblog-eng",
-  apiKey: process.env.API_KEY,
+  apiKey: "c8506c7a403f4df5a77858d8d4789c032224",
 });
