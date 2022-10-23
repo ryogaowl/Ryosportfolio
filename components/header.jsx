9726427c90099b4c1c0a848e3bd6391f1ed2d743
Ryo <ryogassk424@gmail.com>
2022-10-23 19:42:28 +0900

    ✨ ハンバーガーメニュー

--- components/header.jsx
+++ components/header.jsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import React from "react";
+import { useState } from "react";
 
 export function Header() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
   return (
     <div className="sticky top-0 z-50">
       <header className=" w-full pt-5 block pb-5 shadow-xl bg-blue-900 box-border opacity-95">
@@ -9,33 +11,45 @@ export function Header() {
           <div className="text-white">
             <h1 className=" font-bold text-3xl translate">Ryo Portfolio</h1>
           </div>
-          <ul className="flex pt-1 text-white text-xl ">
-            <li className="mr-3 hover:opacity-60 ">
-              <Link href="/">
-                <a>HOME</a>
-              </Link>
-            </li>
-            <li className="mr-3 hover:opacity-60">
-              <Link href="/about">
-                <a>ABOUT</a>
-              </Link>
-            </li>
-            <li className="mr-3 hover:opacity-60">
-              <Link href="/blog">
-                <a>BLOG</a>
-              </Link>
-            </li>
-            <li className="mr-3 hover:opacity-60">
-              <Link href="/works">
-                <a>WORKS</a>
-              </Link>
-            </li>
-            <li className="hover:opacity-60">
-              <Link href="/contact">
-                <a>CONTACT</a>
-              </Link>
-            </li>
-          </ul>
+          <div className="flex">
+            <ul className="pt-1 text-white text-xl md:flex  hidden">
+              <li className="mr-3 hover:opacity-60 ">
+                <Link href="/">
+                  <a>HOME</a>
+                </Link>
+              </li>
+              <li className="mr-3 hover:opacity-60">
+                <Link href="/about">
+                  <a>ABOUT</a>
+                </Link>
+              </li>
+              <li className="mr-3 hover:opacity-60">
+                <Link href="/blog">
+                  <a>BLOG</a>
+                </Link>
+              </li>
+              <li className="mr-3 hover:opacity-60">
+                <Link href="/works">
+                  <a>WORKS</a>
+                </Link>
+              </li>
+              <li className="hover:opacity-60">
+                <Link href="/contact">
+                  <a>CONTACT</a>
+                </Link>
+              </li>
+            </ul>
+            <div className="text-white">
+              <button className={isNavOpen ? "hidden" : "block"}>
+                <svg
+                  className=" pt-1 h-10 w-6 fill-current flex  md:hidden"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
+                </svg>
+              </button>
+            </div>
+          </div>
         </div>
       </header>
     </div>
