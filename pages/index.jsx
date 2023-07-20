6cb698d36b5c06e183783cd0cf59820d747ce060
Ryo <ryogassk424@gmail.com>
2023-07-20 13:01:44 +0900

    🐛 動画再生

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,26 +13,28 @@ export const getStaticProps = async () => {
 
 export default function Home() {
   return (
-    <div className="bg-base relative overflow-hidden">
+    <div className="bg-base relative h-screen overflow-y-hidden">
       <Header />
 
       <div className="relative">
         <video
-          src="public/mov/AdobeStock_131248296.mp4"
-          className="hidden md:block top-0 left-0 right-0 bottom-0 fixed object-cover z-0 h-screen w-screen"
+          src="mov/AdobeStock_131248296.mp4"
+          className=" hidden md:block top-0 left-0 right-0 bottom-0 fixed  object-cover z-0 h-screen w-screen"
           autoPlay
           muted
           playsInline
           loop
         ></video>
-        <video
-          src="mov/AdobeStock_521078998 (1) (1).mp4"
-          className="block md:hidden min-h-screen min-w-screen top-0 left-0 right-0 bottom-0 fixed object-cover z-0"
-          autoPlay
-          muted
-          playsInline
-          loop
-        ></video>
+        <div className="">
+          <video
+            src="mov/AdobeStock_521078998 (1) (1).mp4"
+            className=" block md:hidden min-h-screen min-w-screen top-0 left-0 right-0 bottom-0  relative  object-cover z-0"
+            autoPlay
+            muted
+            playsInline
+            loop
+          ></video>
+        </div>
 
         <div className="text-white fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 animate-text-focus-in md:mt-0 mt-10 z-10">
           <div className="">
