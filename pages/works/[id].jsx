d4947df23437ccadf6a50c313c23f4f433b51555
Ryo <ryogassk424@gmail.com>
2023-08-11 11:10:42 +0900

    ⚡️ day.jsを追加し、日にちの表示を改善

--- pages/works/[id].jsx
+++ pages/works/[id].jsx
@@ -2,7 +2,12 @@ import { Footer } from "../../components/footer";
 import { Header } from "../../components/header";
 import { client } from "../../libs/client";
 import styles from "../../styles/Home.module.scss";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
 
+dayjs.extend(utc);
+dayjs.extend(timezone);
 //SSG
 export const getStaticProps = async (context) => {
   const id = context.params.id;
@@ -25,13 +30,17 @@ export const getStaticPaths = async () => {
   };
 };
 
-export default function WorksId({ works }) {
+export default function WorksId({ works, convertDate }) {
+  const publishedAt = dayjs(convertDate).format("YYYY.MM.DD");
   return (
     <div className="absolute bg-base">
       <Header />
       <main className={styles.main}>
         <h1 className={styles.title}>{works.title}</h1>
-        <p className={styles.publishedAt}>{works.publishedAt}</p>
+        <p className={styles.publishedAt} dateTime={convertDate}>
+          {publishedAt}
+        </p>
+
         <div
           dangerouslySetInnerHTML={{ __html: `${works.body}` }}
           className={styles.post}
